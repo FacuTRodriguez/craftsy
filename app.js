fc9a276575f24351c2c1f3f8f3025bee54eb30a4
Facu <facutomasr@gmail.com>
2023-11-02 11:02:12 -0300

    add style

--- app.js
+++ app.js
@@ -13,6 +13,10 @@ app.get('/register', (req, res)=> res.sendFile(path.join(__dirname, 'views', 're
 app.get('/login', (req, res) =>
   res.sendFile(path.join(__dirname, 'views', 'login.html'))
 )
+app.get('/profile', (req, res) =>
+  res.sendFile(path.join(__dirname, 'views', 'profile.html'))
+)
+
 
 
 
